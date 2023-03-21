3a8804dd73edbbd3e3ec9acb2986338a6335d3e0
janoth123 <70852198+janoth123@users.noreply.github.com>
2023-03-21 14:42:56 +0100

    added jest to test functions

--- App.test.js
+++ App.test.js
@@ -1,9 +1,19 @@
-import { render, screen } from "@testing-library/react";
-import App from './App';
+// import { render, screen } from "@testing-library/react";
+// import App from './App';
+import UserDetails from "./src/components/pages/UserDetails";
 
-test('renders learn react link', () => {
-  render(<App />)
+// test('renders learn react link', () => {
+//   // render(<App />)
+//   render(<  Component/>)
+//   const linkElement = screen.getByText(/learn react/i)
+//   expect(linkElement).toBeInTheDocument()
+// })
 
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
+
+test('user length is 100', async () => {
+  await expect(UserDetails()).resolves.toBe(100);
+});
+
+test('the fetch fails with an error', async () => {
+  await expect(fetchData()).rejects.toMatch('error');
+});
